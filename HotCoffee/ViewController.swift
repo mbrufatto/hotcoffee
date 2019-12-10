afedc4239208c6c01c7b79ba388e4722efb5dff9
Márcio Habigzang Brufatto <mhbrufatto@gmail.com>
2019-12-10 12:56:06 -0300

    Initial Commit

---
+++ HotCoffee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HotCoffee
+//
+//  Created by Marcio Brufatto on 10/12/19.
+//  Copyright © 2019 Marcio Brufatto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
